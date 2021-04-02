5e31576f5360e5437d48056386b96bedf5b44a3c
KMcCrear <kmccrear1@gmail.com>
2021-04-02 19:23:09 +0100

    Profile Complete, just needs to be styled

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AboutUs() {
+	const [firstName, setFirstName] = useState();
+	const [surName, setSurName] = useState();
+	const [email, setEmail] = useState();
+	const [selectOption, setSelectOption] = useState();
+	const [userMessage, setMessage] = useState();
+
+	const contactUs = (e) => {
+		e.preventDefault();
+		let formattedString = `
+		First Name: ${firstName} \n
+		Surname: ${surName} \n
+		Email: ${email} \n
+		Heard From: ${selectOption} \n
+		Message: ${userMessage}`;
+		alert(formattedString);
+	};
+
 	return (
-		<div className="aboutUseContainer">
-			<div className="aboutUS">
-				<div className="aboutUseHeader">
+		<div className="aboutUsContainer">
+			<div className="aboutUs">
+				<div className="aboutUsHeader">
 					<header>
 						<h1>More About Us</h1>
+						<h3>Some Information About Me and This App</h3>
 					</header>
 				</div>
 				<aside>
-					<div className="aboutUseInfo">
-						<h2>Some Information About Me and This App</h2>
+					<div className="aboutUsInfo">
 						<p>
 							Hello my name is Kyle McCrear, I am currently a Glasgow Caledonian
-							University student studying Software Development for Business
+							University student studying Software Development for Business.{" "}
+							<br /> <br /> I'm looking to become a software engineer once I
+							have finished university.
+							<br /> Currently I'm looking to take part in some internships and
+							get experience as a developer before completing my degree.
+						</p>
+						<p>
+							This app has been developed as part of my Web Application
+							Development Courswork.
 						</p>
 					</div>
 				</aside>
@@ -22,21 +47,34 @@ export default function AboutUs() {
 					<div className="contactUs">
 						<fieldset>
 							<form>
-								<label>First Name</label>
-								<input type="text" required />
-								<label>Surname</label>
-								<input type="text" required />
-								<label>Email</label>
-								<input type="email" required />
+								<label>First Name - </label>
+								<input
+									type="text"
+									required
+									onChange={(e) => setFirstName(e.target.value)}
+								/>
+								<label>Surname - </label>
+								<input
+									type="text"
+									required
+									onChange={(e) => setSurName(e.target.value)}
+								/>
+								<label>Email - </label>
+								<input
+									type="email"
+									required
+									onChange={(e) => setEmail(e.target.value)}
+								/>
 								<label>How did you hear about us?</label>
-								<select>
+								<select onChange={(e) => setSelectOption(e.target.value)}>
 									<option defaultValue=""></option>
 									<option>Word of Mouth</option>
 									<option>University</option>
 									<option>Other</option>
 								</select>
-								<label>Message Us</label>
-								<input type="text" />
+								<label>Send Us a Message</label>
+								<textarea onChange={(e) => setMessage(e.target.value)} />
+								<button onClick={contactUs}>Send</button>
 							</form>
 						</fieldset>
 					</div>
